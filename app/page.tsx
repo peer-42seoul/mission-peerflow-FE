f1bc2168c7078f5d1037dc6f62f913448a26fed0
Devicii <62472550+asroq1@users.noreply.github.com>
2023-06-27 14:27:37 +0900

    update: more logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+'use client'
+
+import { useState } from 'react'
 import WritingForm from './WritingForm'
+import EditForm from '@/components/EditForm'
 
 export default function Home() {
-  return (
-    <>
-      <WritingForm />
-    </>
-  )
+  let qid = 42
+  const [test, setTest] = useState(true)
+  return <>{test ? <WritingForm /> : <EditForm questionId={qid} />}</>
 }
