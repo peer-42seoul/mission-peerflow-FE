cee26750a7476fe4d9aff80a6f6428bbed5ababd
Devicii <62472550+asroq1@users.noreply.github.com>
2023-07-02 16:35:49 +0900

    update: bug fixing

--- app/edit/[id]/page.tsx
+++ app/edit/[id]/page.tsx
@@ -1,42 +1,64 @@
 'use client'
+
+import updateQuestion from '../../../api/updateQuestion'
+import useInput from '../../../hooks/useInput'
 import {
   Button,
-  Container,
   FormControl,
   InputLabel,
   MenuItem,
   Select,
-  SelectChangeEvent,
+  Stack,
   TextField,
 } from '@mui/material'
-import Stack from '@mui/material/Stack'
+import { useCallback, useEffect, useState } from 'react'
+import DeleteAndEditModal from '../../../components/DeleteAndEditModal'
 import axios, { AxiosResponse } from 'axios'
-import React, { useCallback, useContext, useEffect, useState } from 'react'
-import useInput from '../../hooks/useInput'
-import GnbContext from '../../hooks/GnbContext'
+import { WritingForm } from '../../../types/WritingForm'
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+}
+
+// 임시로 questionId을 활용해 api 테스트
+
+const questionId = 42
 
 const Page = () => {
-  const { setGnb } = useContext(GnbContext)
-
-  useEffect(() => {
-    setGnb({ title: '새 글', back: true, add: false })
-  }, [])
+  const [questionData, setQuestionData] = useState({})
   const [title, changeTitle] = useInput('')
   const [nickname, changeNickname] = useInput('')
   const [password, changePassword] = useInput('')
   const [content, changeContent] = useInput('')
+  const [name, setName] = useState('')
+  const [showError, setShowError] = useState(false)
   const [category, setCategory] = useState<WritingForm['category'][]>([
     { value: 1, name: 'Minishell' },
     { value: 2, name: 'Minirt' },
     { value: 3, name: 'Fdf' },
   ])
-
-  const [name, setName] = useState('')
-
-  const [showError, setShowError] = useState(false)
+  const [open, setOpen] = useState(false)
+  const [action, setAction] = useState('')
+  const handleOpen = (action) => {
+    setAction(action)
+    setOpen(true)
+  }
+  const handleClose = () => setOpen(false)
 
   const categoryHandler = useCallback((e: SelectChangeEvent) => {
     setName(e.target.value)
+  }, [])
+
+  const deleteHandler = useCallback((e: MouseEvent<HTMLButtonElement>) => {
+    axios.delete(`/v1/question/${questionId}`)
   }, [])
 
   const submitHnadler = useCallback(
@@ -55,33 +77,42 @@ const Page = () => {
         setShowError(true)
         return
       }
-      console.log(
-        `Data: ${title}, ${
-          category.find((item) => item.value === name)?.value
-        }, ${nickname}, ${password}, ${content}`,
-      )
 
-      axios
-        .post('/v1/question', {
-          type: 'question',
+      setQuestionData((prevQuestionData) => {
+        const updatedQuestionData = {
+          ...prevQuestionData,
           title,
           nickname,
           password,
           content,
-          category: category.find((item) => item.value === name)?.value,
-        })
-        .then((res: AxiosResponse<any>) => {
-          console.log(`res : ${res}`)
-        })
-        .catch((err) => {
-          console.log(`err ${err}`)
-        })
+          category: category.find((item) => item.value.toString() === name)
+            ?.value,
+        }
+        return updatedQuestionData
+      })
+      updateQuestion({ questionId: questionId, data: questionData })
     },
     [title, nickname, password, content, category],
   )
 
+  useEffect(() => {
+    axios
+      .get(`/v1/question${questionId}`)
+      .then((res: string) => {
+        console.log(`res : ${res}`)
+        changeTitle(res.title)
+        changeNickname(res.nickname)
+        changePassword(res.password)
+        changeContent(res.content)
+        setCategory(res.value)
+      })
+      .catch((err) => {
+        console.log(`err ${err}`)
+        alert('불러오기에 실패하였습니다.')
+      })
+  }, [])
   return (
-    <Container>
+    <>
       <form onSubmit={submitHnadler}>
         <Stack
           direction="column"
@@ -94,6 +125,7 @@ const Page = () => {
             label="제목"
             fullWidth
             onChange={changeTitle}
+            defaultValue={title}
           />
           {showError && title.trim().length === 0 && (
             <div style={{ color: 'red' }}>제목을 입력해주세요.</div>
@@ -128,11 +160,10 @@ const Page = () => {
                 name="nickname"
                 onChange={changeNickname}
                 style={{ width: '100%' }}
+                defaultValue={nickname}
               />
               {showError && nickname.trim().length === 0 && (
-                <div style={{ color: 'red', marginTop: '16px' }}>
-                  닉네임을 입력해주세요.
-                </div>
+                <div style={{ color: 'red' }}>닉네임을 입력해주세요.</div>
               )}
             </Stack>
             <Stack width={'45%'}>
@@ -141,12 +172,11 @@ const Page = () => {
                 placeholder="비밀번호"
                 name="password"
                 onChange={changePassword}
+                defaultValue={password}
                 style={{ width: '100%' }}
               />
               {showError && password.trim().length === 0 && (
-                <div style={{ color: 'red', marginTop: '16px' }}>
-                  비밀번호를 입력해주세요.
-                </div>
+                <div style={{ color: 'red' }}>비밀번호를 입력해주세요.</div>
               )}
             </Stack>
           </Stack>
@@ -154,7 +184,7 @@ const Page = () => {
             id="outlined-multiline-static"
             multiline
             rows={4}
-            // defaultValue={title}
+            defaultValue={content}
             fullWidth
             onChange={changeContent}
           />
@@ -167,13 +197,31 @@ const Page = () => {
             alignItems="center"
             spacing={2}
           >
-            <Button type="submit" variant="outlined">
-              작성하기
+            <Button
+              type="button"
+              variant="outlined"
+              onClick={() => showError === false && handleOpen('수정')}
+            >
+              수정하기
+            </Button>
+            <Button
+              color="error"
+              type="button"
+              variant="outlined"
+              onClick={() => !showError && handleOpen('삭제')}
+            >
+              삭제하기
             </Button>
           </Stack>
+          <DeleteAndEditModal
+            open={open}
+            handleClose={handleClose}
+            evtHandler={action === '수정' ? submitHnadler : deleteHandler}
+            action={action}
+          />
         </Stack>
       </form>
-    </Container>
+    </>
   )
 }
 
